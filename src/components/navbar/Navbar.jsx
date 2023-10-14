53257bc870115eff92a80a8ebbf0327fa70c1890
H M Rizwan Mazumder <riz1.xtreme@gmail.com>
2023-10-14 23:54:51 +0530

    Commit 3: Navbar Animation

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,17 @@
 import "./navbar.scss"
+import {motion} from "framer-motion";
 
 const Navbar = () => {
   return (
     <div className="navbar">
         {/* Sidebar */}
         <div className="wrapper">
-            <span>Techno3Gamma | Rizwan</span>
+            <motion.span 
+              initial={{opacity:0, scale:0.5}} 
+              animate={{opacity:1, scale:1}} 
+              transition={{duration: 0.5}}
+            >
+              Techno3Gamma | Rizwan</motion.span>
             {/* <span><a href="">Home</a></span> */}
             <div className="social">
                 <a href="#"><img src="/facebook.png" alt="" /></a>
