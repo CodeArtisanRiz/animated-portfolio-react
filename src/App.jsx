08073231347726e7c4398f5b805c7b0e2870cb55
H M Rizwan Mazumder <riz1.xtreme@gmail.com>
2023-10-14 23:38:13 +0530

    Commit 2: Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import Test from "./Test";
 import "./app.scss"
+import Navbar from "./components/navbar/Navbar";
 
 const App = () => {
   return <div>
-    <section>Hero</section>
+    <section>
+      <Navbar/>
+    </section>
     <section>Parallax</section>
     <section>Services</section>
     <section>Parallax</section>
@@ -10,6 +14,7 @@ const App = () => {
     <section>Portfolio2</section>
     <section>Portfolio3</section>
     <section>Contact</section>
+    {/* <Test/> */}
   </div>;
 };
 
