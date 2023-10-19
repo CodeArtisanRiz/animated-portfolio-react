5c3ce89f49ef1209f417e75748f3a7e94804abe8
H M Rizwan Mazumder <riz1.xtreme@gmail.com>
2023-10-19 13:12:25 +0530

    Commit 5: Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import Test from "./Test";
 import "./app.scss"
 import Navbar from "./components/navbar/Navbar";
+import Hero from "./components/hero/Hero";
 
 const App = () => {
   return <div>
-    <section>
+    <section id="Homepage">
       <Navbar/>
+      <Hero/>
     </section>
     <section>Parallax</section>
     <section>Services</section>
---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,43 @@
+import "./hero.scss";
+import { motion } from "framer-motion";
+
+const textVariants = {
+    initial:{
+        x: -500,
+        opacity: 0,
+    },
+    animate:{
+        x: 0,
+        opacity: 1,
+        transition:{
+            duration: 1,
+            staggerChilden: 0.1,
+        },
+    },
+}
+
+const Hero = () => {
+  return (
+    <div className="hero">
+        <div className="wrapper">
+            <motion.div className="textContainer" variants={textVariants}>
+                <motion.h2 variants={textVariants}>H M Rizwan Mazumder</motion.h2>
+                <motion.h1 variants={textVariants}>Web Developer & UI Designer</motion.h1>
+                <motion.div variants={textVariants} className="buttons">
+                    <motion.button variants={textVariants}>See the Latest Works.</motion.button>
+                    <motion.button variants={textVariants}>Contact Me</motion.button>
+                </motion.div>
+                <motion.img variants={textVariants} src="./scroll.png" alt="" />
+            </motion.div>
+        </div>
+        <div className="slidingTextContainer">
+            Android iOS React Java Developer 
+        </div>
+        <div className="imageContainer">
+            <img src="/hero1.png" alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default Hero
