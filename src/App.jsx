136a32ced1949dcaa194f19d04aca9b7795c517c
H M Rizwan Mazumder <riz1.xtreme@gmail.com>
2023-10-28 21:44:54 +0530

    Commit 19: Mobile Responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import Hero from "./components/hero/Hero";
 import Parallax from "./components/parallax/Parallax";
 import Services from "./components/services/Services";
 import Portfolio from "./components/portfolio/Portfolio";
-import Contact from "./components/contact/Contact"
+import Contact from "./components/contact/Contact";
+import Cursor from "./components/cursor/Cursor";
 
 const App = () => {
   return <div>
+    <Cursor/>
     <section id="Homepage">
       <Navbar/>
       <Hero/>
@@ -17,7 +19,7 @@ const App = () => {
     <section id="Portfolio"><Parallax type="portfolio"/></section>
     <Portfolio/>
     <section id="Contact"><Contact/></section>
-    {/* <Test/> */}
+    
   </div>;
 };
 
